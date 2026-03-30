c8dae64a5b591117025964bf7d2ed0a713281ea5
Riddhi Srivastava <riddhisrivastava2003@gmail.com>
2026-03-30 15:50:53 +0530

    added microservices architecture

---
+++ quantity-service/src/main/java/com/riddhi/quantity_service/QuantityServiceApplication.java
@@ -0,0 +1,14 @@
+package com.riddhi.quantity_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+  // ✅ Correct annotation
+@SpringBootApplication
+public class QuantityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuantityServiceApplication.class, args);
+	}
+}
